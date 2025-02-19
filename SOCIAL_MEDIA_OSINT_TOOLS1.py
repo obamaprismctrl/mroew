735d283960d56b16781d006279648376d7f36014
obamaprismctrl <112917990+obamaprismctrl@users.noreply.github.com>
2025-02-19 00:17:01 -0700

    Add files via upload

---
+++ SOCIAL_MEDIA_OSINT_TOOLS1.py
@@ -0,0 +1,72 @@
+SOCIAL_MEDIA_OSINT_TOOLS = {
+    #"True_People_Search": {
+     #   "Profile_search": [
+      #      {"name": "Truepplsearch", "link": "https://www.truepeoplesearch.com/results?name={first_name}%20{sirname}"},
+       # ] 
+   # },
+    "Pinterest": {
+        "Profile_search": [
+            {"name": "Pintrest", "link": "https://www.pinterest.com/search/{username}"},
+            {"name": "Pintrest Follower Analysis", "link": "hhttps://www.pinterest.com/{username}/followers/"},
+            {"name": "Pingroupie", "link": "https://realwebsite.com/pingroupie/{username}"},
+            {"name": "Tailwind", "link": "https://realwebsite.com/tailwind/{username}"},
+            {"name": "Pinterest Guest", "link": "https://realwebsite.com/pinterest-guest/{username}"},
+            {"name": "SourcingLab: Pinterest", "link": "https://realwebsite.com/sourcinglab-pinterest/{username}"}
+        ]
+    },
+    "Reddit": {
+        "Profile_search": [
+            {"name": "Reddit", "link": "https://www.reddit.com/search/{username}"},
+            {"name": "redective", "link": "https://www.redective.com/"},
+            {"name": "Mostly Harmless", "link": "hhttps://www.reveddit.com/y/{username}/?all=true&removal_status=all"},
+            {"name": "OSINT Combine: Reddit Post Analyzer", "link": "https://realwebsite.com/osint-combine-reddit-post-analyzer/{username}"},
+            {"name": "Phantom Buster", "link": "https://realwebsite.com/phantom-buster/{username}"},
+        ]
+    },
+    "Snapchat": {
+        "Profile_search": [
+            {"name": "Snapchat", "link": "https://www.snapchat.com/explore/{username}"},
+            {"name": "AddMeSnaps", "link": "https://www.social-searcher.com/social-buzz/?q5={username}"},
+            {"name": "Gebruikersnamen: Snapchat", "link": "https://realwebsite.com/gebruikersnamen-snapchat/{username}"},
+        ]
+    },
+    "WhatsApp": {
+        "Profile_search": [
+            {"name": "checkwa", "link": "https://realwebsite.com/checkwa/{username}"},
+            {"name": "WhatsApp Fake Chat", "link": "https://realwebsite.com/whatsapp-fake-chat/{username}"},
+            {"name": "Whatsapp Monitor", "link": "https://realwebsite.com/whatsapp-monitor/{username}"},
+            {"name": "whatsfoto", "link": "https://realwebsite.com/whatsfoto/{username}"}
+        ]
+    },
+    "Skype": {
+        "Profile_search": [
+            {"name": "addmeContacts", "link": "https://realwebsite.com/addmecontacts/{username}"},
+            {"name": "ChatToday", "link": "https://realwebsite.com/chattoday/{username}"},
+            {"name": "Skypli", "link": "https://realwebsite.com/skypli/{username}"}
+        ]
+    },
+    "Telegram": {
+        "Profile_search": [
+            {"name": "Telegram #1", "link": " t.me/USERNAME/{username}"},
+            {"name": "Telegram #2", "link": "web.telegram.org/k/#@{username}"},
+            {"name": "Telegram #3", "link": "web.telegram.org/z/#@{username}"},
+            {"name": "Lyzem", "link": "https://lyzem.com/search?q=mike"},
+        ]
+    },
+    "OnlyFans": {
+        "Profile_search": [
+            {"name": "OnlyFinder", "link": "https://onlyfinder.com/{username}"},
+            {"name": "OnlySearch", "link": "https://onlysearch.co/profiles?keyword={username}"},
+            {"name": "Fansmetrics", "link": "https://fansmetrics.co/?q={username}"},
+            {"name": "Findr.fans", "link": "https://findr.fans/search?type=article%252Cpage%252Cproduct&q={username}"},
+            {"name": "Hubite", "link": "https://hubite.com/en/onlyfans-search/?q={username}"},
+            {"name": "Fansearch", "link": "Fansearch"},
+            {"name": "Fulldp", "link": "https://fulldp.co/onlyfans-full-size-profile-picture/?id=https://onlyfans.com/{username}"}
+        ]
+    },
+    "TikTok": {
+        "Profile_search": [
+            {"name": "Tiktok Search", "link": "https://www.tiktok.com/@{username}"},
+        ]
+    }
+}
